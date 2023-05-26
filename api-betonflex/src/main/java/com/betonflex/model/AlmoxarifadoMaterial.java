4141c5756ad8de79b17e37ce6a3ea1b46888cbe8
robertosxav <roberto.sxav@gmail.com>
2023-05-26 15:31:03 -0400

    mapeando campos novos almoxarifadoMaterial

--- api-betonflex/src/main/java/com/betonflex/model/AlmoxarifadoMaterial.java
+++ api-betonflex/src/main/java/com/betonflex/model/AlmoxarifadoMaterial.java
@@ -1,8 +1,14 @@
 package com.betonflex.model;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
@@ -31,7 +37,29 @@ public class AlmoxarifadoMaterial implements Serializable{
 	@ManyToOne(cascade = CascadeType.ALL) 
 	@JoinColumn(name = "material_id")
 	private Material material;
-
+	
+	@Column(name="almoxarifado_material_lote")
+	private String lote;
+	
+	@Column(name="almoxarifado_material_valor_un")
+	private BigDecimal valorUnitario;
+	
+	@Column(name="almoxarifado_material_valor_to")
+	private BigDecimal valorTotal;
+	
+	@Column(name="almoxarifado_material_qtd")
+	private BigDecimal qtde;
+	
+	@Column(name="almoxarifado_material_qtd_uti")
+	private BigDecimal qtdeUtilizada;
+	
+	@Column(name="almoxarifado_material_status")
+	private String status;
+	
+	@JsonFormat(pattern="dd/MM/yyyy")
+	@Column(name="almoxarifado_material_data")
+	private LocalDate data;
+	
 	public Long getAlmoxarifadoMaterialId() {
 		return almoxarifadoMaterialId;
 	}
@@ -56,6 +84,62 @@ public class AlmoxarifadoMaterial implements Serializable{
 		this.material = material;
 	}
 
+	public String getLote() {
+		return lote;
+	}
+
+	public void setLote(String lote) {
+		this.lote = lote;
+	}
+
+	public BigDecimal getValorUnitario() {
+		return valorUnitario;
+	}
+
+	public void setValorUnitario(BigDecimal valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+
+	public BigDecimal getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(BigDecimal valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	public BigDecimal getQtde() {
+		return qtde;
+	}
+
+	public void setQtde(BigDecimal qtde) {
+		this.qtde = qtde;
+	}
+
+	public BigDecimal getQtdeUtilizada() {
+		return qtdeUtilizada;
+	}
+
+	public void setQtdeUtilizada(BigDecimal qtdeUtilizada) {
+		this.qtdeUtilizada = qtdeUtilizada;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	
 	public AlmoxarifadoMaterial() {
 		super();
 		// TODO Auto-generated constructor stub
@@ -66,7 +150,19 @@ public class AlmoxarifadoMaterial implements Serializable{
 		this.almoxarifado = almoxarifado;
 		this.material = material;
 	}
-	
-	
+
+	public AlmoxarifadoMaterial(Almoxarifado almoxarifado, Material material, String lote, BigDecimal valorUnitario,
+			BigDecimal valorTotal, BigDecimal qtde, BigDecimal qtdeUtilizada, String status, LocalDate data) {
+		super();
+		this.almoxarifado = almoxarifado;
+		this.material = material;
+		this.lote = lote;
+		this.valorUnitario = valorUnitario;
+		this.valorTotal = valorTotal;
+		this.qtde = qtde;
+		this.qtdeUtilizada = qtdeUtilizada;
+		this.status = status;
+		this.data = data;
+	}
 	 
 } 
